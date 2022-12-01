52022393f84435cea78637d043f2e7328e63da10
Andreas <andreas.kissavos@equa.se>
2022-12-01 07:18:31 +0100

    2022 1 1

---
+++ prob-4/src/prob_4/2022-1.clj
@@ -0,0 +1,21 @@
+(ns src.prob-4.2022-1
+  
+  (:require [clojure.string :as str]))
+
+
+(def input-file "d:\\programming\\aoc2021\\prob-4\\input-2022-1.txt")
+
+(def test-input "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000")
