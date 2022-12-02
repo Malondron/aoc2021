67db8c5a3d7e492d85533408ff9d75c4125b4d31
Andreas <andreas.kissavos@equa.se>
2022-12-02 06:54:23 +0100

    2022 2 2

--- prob-4/src/prob_4/2022-2.clj
+++ prob-4/src/prob_4/2022-2.clj
@@ -36,3 +36,47 @@ C Z")
     
     )
   )
+
+(defn solv1 [rounds points]
+  (if (= 0 (count rounds))
+    points
+    (recur (rest rounds) (+ points (calcRoundPoints (first rounds))))
+    )
+  )
+
+(solv1 (parse-input test-input) 0)
+(solv1 (parse-input (slurp input-file)) 0)
+
+
+(defn calcRoundPoints-2 [round]
+  (let [opp (first round)
+        me (second round)
+        ]
+    (case opp
+      "A" (case me
+            "X" (+ 0 3)
+            "Y" (+ 3 1)
+            "Z" (+ 6 2))
+      "B" (case me
+            "X" (+ 0 1)
+            "Y" (+ 3 2)
+            "Z" (+ 6 3))
+       "C" (case me
+            "X" (+ 0 2)
+            "Y" (+ 3 3)
+            "Z" (+ 6 1))
+            
+      )
+    
+    )
+  )
+
+(defn solv2 [rounds points]
+  (if (= 0 (count rounds))
+    points
+    (recur (rest rounds) (+ points (calcRoundPoints-2 (first rounds))))
+    )
+  )
+
+(solv2 (parse-input test-input) 0)
+(solv2 (parse-input (slurp input-file)) 0)
