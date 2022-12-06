fd1ee7a26829c05cf1e72404458a349351dff55a
Andreas <andreas.kissavos@equa.se>
2022-12-06 10:02:31 +0100

    2022 6 2

--- prob-4/src/prob_4/2022-6.clj
+++ prob-4/src/prob_4/2022-6.clj
@@ -20,4 +20,16 @@
 (findStartOfMarker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" [] 0)
 (findStartOfMarker (slurp input-file) [] 0)
 
-(cons \h [])
+(defn findStartOfMessage [chars lastfour pos]
+  (if (= 0 (count chars))
+    -1
+    (if (and (> pos 13) (= 14 (count (set lastfour))))
+      pos
+      (recur (rest chars) (if (<= pos 13) (cons  (first chars) lastfour) (cons (first chars) (take 13 lastfour) )) (inc pos))
+      )
+    )
+  
+  )
+
+(findStartOfMessage "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" [] 0)
+(findStartOfMessage (slurp input-file) [] 0)
