1070c6320c4923be6a1af9e8e55184b9a3a5dd0e
Andreas <andreas.kissavos@equa.se>
2022-12-06 09:59:17 +0100

    2022 6 1

---
+++ prob-4/src/prob_4/2022-6.clj
@@ -0,0 +1,23 @@
+(ns src.prob-4.2022-6
+  (:require [clojure.string :as str]))
+
+
+
+(def input-file "d:\\programming\\aoc2021\\prob-4\\input-2022-6.txt")
+
+
+(defn findStartOfMarker [chars lastfour pos]
+  (if (= 0 (count chars))
+    -1
+    (if (and (> pos 3) (= 4 (count (set lastfour))))
+      pos
+      (recur (rest chars) (if (<= pos 3) (cons  (first chars) lastfour) (cons (first chars) (take 3 lastfour) )) (inc pos))
+      )
+    )
+  
+  )
+
+(findStartOfMarker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" [] 0)
+(findStartOfMarker (slurp input-file) [] 0)
+
+(cons \h [])
